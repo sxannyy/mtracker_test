ca6d8c5364100f1ff0bf2e13f5f98c8dd3ab9281
sxannyy <sxannyy@mail.ru>
2024-04-23 18:04:09 +0800

    first

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+   name='mtraker',
+   version='1.0',
+   description='Provides a decorator for memory usage tracking. The part of FOSS course.',
+   license='MIT',
+   author='Sasha Sanzhitova',
+   author_email='sxannyy@gmail.com',
+   url='https://github.com/sxannyy/mtracker_test.git',
+   packages=['mtracker'],
+   install_requires=[], # it is empty since we use standard python library
+   extras_require={
+        'test': [
+            'pytest',
+            'coverage',
+        ],
+   },
+   python_requires='>=3',
+)
